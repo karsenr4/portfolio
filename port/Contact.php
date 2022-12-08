16d70c749b5886ced6395ff120772e5eb4b81810
karsenr4 <120101599+karsenr4@users.noreply.github.com>
2022-12-08 04:36:09 -0700

    Add files via upload

---
+++ port/Contact.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+	<head>
+		<meta charset="utf-8">
+		<title>New</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet"  href="styles.css">
+	</head>
+    <header>
+      <div class="head"> 
+        <img class="logo" src="logos/logo_black.png" width="100px" height="100px">
+        <nav>
+          <a href="index.html" class="button">Home</a>
+          <a href="gallery.html" class="button">Gallery</a>
+          <a href="past.php" class="button">About Me</a>
+          <a href="Contact.php" class="button">Contact</a>
+          <div class="social">
+            <img src="social/logo-twitter.svg" width="20px" height="20px">
+            <img src="social/logo-instagram.svg" width="20px" height="20px">
+            </div>
+      </nav>
+    
+          <h1>Karsen Rude Design</h1>
+          </div>
+    </header>
+	<body>
+        <div class="holder">
+            <div class="email">
+        <h3>Somthing</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quas quis, nesciunt consequatur recusandae voluptate aspernatur? Repellendus sed, quaerat sit officia, consequatur qui ea doloremque eos dolor quae soluta doloribus.</p>
+        <p>Karsenr4@gmail.com</p>
+
+</div>
+
+    <div class="container">
+  <form action="action_page.php">
+
+    <div class="name">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" >
+    
+    </div>
+    <div class="name">
+    <label class="last" for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" >
+    
+</div>
+<div class="continfo">
+<label class="label" for="Email">Email</label>
+    <input class="text" type="text" id="Email" name="Email" >
+    
+
+    <label class="label" for="subject">Subject</label>
+    <input class="text" id="subject" name="subject" >
+    
+    
+    <label class="label" for="message">Message</label>
+    <textarea class="text" id="message" name="message" ></textarea>
+    
+    <input class="btn" type="submit" value="Submit">
+</div>
+  </form>
+</div>
+</div>
+    </body>
+
+</html>
